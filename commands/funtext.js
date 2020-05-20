dd1a13d204e9883898ce9efe32e2d1441741db74
Daniel Noon <danielanoon@outlook.com>
2020-05-20 15:02:24 -0500

    be better

--- commands/funtext.js
+++ commands/funtext.js
@@ -22,13 +22,13 @@ module.exports = {
   "corner": (params, msg) => {
     msg.channel.send((x => x.split("").map((a, b) => ((b == 0) ? x + "\n" : a + "\n")).join(""))(params.join(" "))).then(() => msg.delete());
   },
-  "retard": (params, msg) => {
+  "silly": (params, msg) => {
     if (params.length != 0) {
       msg.channel.send((s => ">" + s.split("").map((d, i) => i % 2 ? d.toLowerCase() : d.toUpperCase()).join(""))(params.join(" "))).then(() => msg.delete())
     }
     else {
       let rand = Math.floor(Math.random() * (people.length - 1));
-      msg.reply(people[rand] + " is a retard.");
+      msg.reply(people[rand] + " is silly.");
     }
   }
 }
